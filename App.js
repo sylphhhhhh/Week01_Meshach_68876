3931fc810f67244e46d6ab4f32a5132716b05a1c
sylphhhhhh <153562312+sylphhhhhh@users.noreply.github.com>
2024-09-03 22:32:21 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Meshach Christian - 00000068876</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
